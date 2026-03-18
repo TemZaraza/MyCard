2f15204c8bd457cb2202f4176b71be6aaf1e459d
temzaraza <temzaraza@gmail.com>
2026-03-18 22:28:23 +0300

    hideShow joke button edited, need to make it run smooth

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 let showJoke = false;
+if (!showJoke) {
+    document.getElementById('barbarian-joke').style.display = 'none';
+}
 
-//Make joke hidden until show button is clicked
-
-function hideJoke() {
+function hideShowJoke() {
+    console.log('showJoke = ', showJoke)
     if (showJoke) {
         document.getElementById('barbarian-joke').style.display = 'none';
         document.getElementById("joke-button").innerHTML = "Show joke :)";
     } else {
         document.getElementById('barbarian-joke').style.display = '';
         document.getElementById("joke-button").innerHTML = "Hide joke :|";
+        document.getElementById('end-of-joke').scrollIntoView({behavior: 'smooth'}); 
+        // Why smooth behaviour works only one time after first click?
     }
     showJoke = !showJoke;
 }
