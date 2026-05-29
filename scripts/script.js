a6abc35a2e7e7592724abaed5dc509ce3d75d355
temzaraza <temzaraza@gmail.com>
2026-05-29 21:54:48 +0300

    smooth fade in/out for b-joke added

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,21 @@
-let showJoke = false;
-if (!showJoke) {
-    document.getElementById('barbarian-joke').style.display = 'none';
-}
+Joke(showJoke = false);
 
-function hideShowJoke() {
-    if (showJoke) {
-        document.getElementById('barbarian-joke').style.display = 'none';
-        document.getElementById("joke-button").innerHTML = "Show joke :)";
-    } else {
-        document.getElementById('barbarian-joke').style.display = '';
+function Joke(show) {
+    if (!show) {
+        document.getElementById('barbarian-joke').style.opacity = '0';
+        document.getElementById('joke-button').innerHTML = "Show joke :)";
+        document.getElementById('start-of-page').scrollIntoView({behavior: 'smooth'});
+        document.getElementById('barbarian-joke').style.transition = 'opacity 0.7s 0.2s'
+    } else {        
+        document.getElementById('barbarian-joke').style.opacity = '1';
         document.getElementById("joke-button").innerHTML = "Hide joke :|";
         document.getElementById('end-of-joke').scrollIntoView({behavior: 'smooth'}); 
+        document.getElementById('barbarian-joke').style.transition = 'opacity 0.7s 0.2s'
     }
+}
+
+function hideShowJoke() {  
     showJoke = !showJoke;
+    Joke(showJoke);
 }
 
