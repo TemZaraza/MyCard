504fa29bfdd9bf0bf17065ec57bf61ff3d2a3a0b
temzaraza <temzaraza@gmail.com>
2026-06-11 22:13:39 +0300

    trying to adjust scroll

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ function Joke(show) {
     if (!show) {
         document.getElementById('barbarian-joke').style.opacity = '0';
         document.getElementById('joke-button').innerHTML = "Show joke :)";
-        document.getElementById('start-of-page').scrollIntoView({behavior: 'smooth'});
+        document.getElementById('center-of-page').scrollIntoView({behavior: 'smooth'});
         document.getElementById('barbarian-joke').style.transition = 'opacity 0.7s 0.2s'
     } else {        
         document.getElementById('barbarian-joke').style.opacity = '1';
